d6542f70aab5cbd919e9ddfac4df42b1dff48b03
ritiksr25 <ritiksr25@gmail.com>
2020-05-31 03:36:07 +0530

    --update: integrated to backend-v2-api

--- config/cron-wisher.js
+++ config/cron-wisher.js
@@ -1,4 +1,10 @@
 const cron = require("node-cron");
+const axios = require("axios");
+
+const {
+	GET_BIRTHDAYS_PROCESS_API,
+	GET_BIRTHDAYS_PROCESS_SECRET
+} = require("./index");
 const { sendWishMail } = require("./emailService");
 
 let time = "0 0 * * *"; // everyday 0:0:0
@@ -6,13 +12,23 @@ let scheduled = true,
 	timezone = "Asia/Kolkata";
 
 let tickFunction = async () => {
-	let today = new Date();
-	today = new Date(2020, today.getMonth(), today.getDate() + 1).toUTCString();
-	let wishes = []; // array of objects: { name, email } for birthdays today;
+	let res = await axios.get(GET_BIRTHDAYS_PROCESS_API, {
+		headers: {
+			"x-access-token": GET_BIRTHDAYS_PROCESS_SECRET
+		}
+	});
+
+	console.log(res.data);
+
+	if (!res || !res.data || res.data.message !== "success") {
+		console.log("Something went wrong!!");
+		return;
+	}
+
+	let wishes = res.data.data;
 	console.log("Running cron task!!");
 	let promises = [];
 	wishes.map(wish => {
-		// do stuffs, send mail function, push to promises
 		promises.push(sendWishMail(wish.email, wish));
 		console.log(`Sending wishes to ${wish.name} (${wish.email})`);
 	});
